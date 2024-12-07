c990d2c67e7afaea1cad45b2d6a65e8fb3161988
vansh <vanshsharma250609@gmail.com>
2024-12-08 00:40:01 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tictactoe';
-}
+    cells:string[]=Array(10).fill(null);
+    playersymbol:string='X';
+    ongridcellclick(index:number):void{
+     this.cells[index]=this.playersymbol;
+     if(this.playersymbol=='X'){
+      this.playersymbol='O';
+     }
+     else{
+      this.playersymbol='X';}
+     }
+    }
+
